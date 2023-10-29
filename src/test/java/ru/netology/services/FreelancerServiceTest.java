ddd89f9dff78111729e12a573401f35f33b31d12
Anna Gryazeva <anna_gryazeva@mail.ru>
2023-10-29 22:14:18 +0300

    Initial commit

---
+++ src/test/java/ru/netology/services/FreelancerServiceTest.java
@@ -0,0 +1,28 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class FreelancerServiceTest {
+    @Test
+    public void testCalculateTest1() {
+        FreelancerService service = new FreelancerService();
+
+        int actual = service.calculate(10_000, 3000, 20_000);
+        int expected = 3;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCalculateTest2() {
+        FreelancerService service = new FreelancerService();
+
+        int actual = service.calculate(100_000, 60_000, 150_000);
+        int expected = 2;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
